beade69736dd60048966189df9bef964ade0fe11
Sargaris <Stokini2020@gmail.com>
2021-10-26 20:43:02 +0300

    create conection with two activities

--- app/src/main/java/com/example/dayplannertest/MainActivity.kt
+++ app/src/main/java/com/example/dayplannertest/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.dayplannertest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.dayplannertest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        lateinit var bindingClass: ActivityMainBinding
 
+
+
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(LayoutInflater.from(application))
+        setContentView(bindingClass.root)
+
+
+
+        bindingClass.btStepToNextActivity.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/dayplannertest/SecondActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dayplannertest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.dayplannertest.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var SecBindingClass: ActivitySecondBinding
+
+        super.onCreate(savedInstanceState)
+        SecBindingClass = ActivitySecondBinding.inflate(LayoutInflater.from(application))
+        setContentView(SecBindingClass.root)
+
+        SecBindingClass.btToStepBack.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+
+}
